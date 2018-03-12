ad3ecf5aa844c19e86ca2d20e56660be702fd679
Fiach Reid <fiach.reid@gmail.com>
2018-03-12 17:48:10 +0000

    looky here

---
+++ Ratings/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  Ratings
+//
+//  Created by user909680 on 3/12/18.
+//  Copyright © 2018 user909680. All rights reserved.
+//  https://www.raywenderlich.com/160521/storyboards-tutorial-ios-11-part-1
+
+import Foundation
+
+struct Player {
+    
+    // MARK: - Properties
+    var name: String?
+    var game: String?
+    var rating: Int
+}
---
+++ Ratings/PlayersViewControllerTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PlayersViewControllerTableViewController.swift
+//  Ratings
+//
+//  Created by user909680 on 3/12/18.
+//  Copyright © 2018 user909680. All rights reserved.
+//
+
+import UIKit
+
+class PlayersViewControllerTableViewController: UITableViewController {
+
+    // MARK: - Properties
+    var players = SampleData.generatePlayersData()
+}
+
+// MARK: - UITableViewDataSource
+extension PlayersViewControllerTableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return players.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell", for: indexPath)
+        
+        let player = players[indexPath.row]
+        cell.textLabel?.text = player.name
+        cell.detailTextLabel?.text = player.game
+        return cell
+    }
+}
